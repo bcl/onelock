45b1e5d00fcff0da6cf0ba1bc0051375252b090a
Brian C. Lane <bcl@brianlane.com>
2005-03-21 00:20:59 +0000

    Basic DCO tuning is in place. Not very intelligent, but it times the 32768 crystal and tries to adjust DCOCTL

--- interrupts.c
+++ interrupts.c
@@ -11,15 +11,30 @@
    ----------------------------------------------------------------------- */
 #include "hardware.h"
 
+#define T125_MS	4096
+#define MIN_DCO_TIME 29687
+#define MAX_DCO_TIME 32812
 
+
+short last_tbr;
 
 void InitIRQ( void )
 {
-    /* Setup Timer A0 run off ACLK */
+  /* Setup Timer A0 run off ACLK /1, continuous up count, start */
+  TACTL = TASSEL_ACLK | TACLR;  
+  TACTL |= MC_CONT;
+  
+  /* Setup Timer B0 to run off MCLK (DCO) /4 */
+  TBCTL = TBSSEL_SMCLK | TBCLR | ID_DIV4;
+  TBCTL |= MC_CONT;
     
-    
-    /* Setup an interrupt on TA0 for 125mS (4096 count) */
+  /* Setup an interrupt on TA0 for 125mS (4096 count) */
+  last_tbr = TBR;
+  CCR0 = TAR + T125_MS;
+  CCTL0 = CCIE;  
 
+  /* Enable Interrupts */
+  _EINT();
 }
 
 
@@ -47,6 +62,35 @@ interrupt (TIMERA1_VECTOR) INT_timera1( void )
 
 interrupt (TIMERA0_VECTOR) INT_timera0( void )
 {
+  unsigned short t;
+  
+  /* Stop Timer B */
+  TBCTL = TBSSEL_SMCLK | ID_DIV4;
+  
+  t = TBR;
+  CCR0 += T125_MS;
+
+  /* Figure out how fast the DCO is running */
+  if( t < MIN_DCO_TIME )
+  {
+    /* The DCO is running too slow */
+    if( DCOCTL < 0xFF )
+    {
+      DCOCTL += 1;
+    }
+  } else if( t > MAX_DCO_TIME ) {
+    /* The DCO is running too fast */
+    if( DCOCTL > 0 )
+    {
+      DCOCTL -= 1;
+    }
+  } else {
+    asm("  nop");
+  }
+
+  /* Clear and restart Timer B */
+  TBCTL |= TBCLR;
+  TBCTL |= MC_CONT;
 }
 
 interrupt (ADC_VECTOR) INT_adc( void )
